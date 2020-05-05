7bc6c1b477b5d15b2a9fe0d1ae4488ac6f685dbc
Sterlingclark <sterlingdclark13@gmail.com>
2020-05-04 22:38:37 -0400

    added mars and ship to background

---
+++ listing1-2.py
@@ -0,0 +1,13 @@
+# Spacewalk 
+# Sterling Clark 
+# https://github.com/Sterlingclark
+
+WIDTH = 800 
+HEIGHT = 600
+player_x = 600
+player_y = 350
+
+def draw():
+  screen.blit(images.backdrop, (0, 0))
+  screen.bilt(images.mars, (50, 50))
+  screen.bilt(images.ship (130, 150))
