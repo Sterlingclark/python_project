bf9c0a0240eef40035ae4610d5a85c829338b52c
Sterlingclark <sterlingdclark13@gmail.com>
2020-05-04 22:48:37 -0400

    adding mars and ship

--- listing1-2.py
+++ listing1-2.py
@@ -2,12 +2,12 @@
 # Sterling Clark 
 # https://github.com/Sterlingclark
 
-WIDTH = 800 
+WIDTH = 800
 HEIGHT = 600
-player_x = 600
-player_y = 350
+player_x = 500
+player_y = 550
 
 def draw():
-  screen.blit(images.backdrop, (0, 0))
-  screen.bilt(images.mars, (50, 50))
-  screen.bilt(images.ship (130, 150))
+    screen.blit(images.backdrop, (0, 0))
+    screen.blit(images.mars, (50, 50))
+    screen.blit(images.ship, (130, 150))
