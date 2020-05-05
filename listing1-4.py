0fe7a01963a49f046223784b1ce228541497cd69
Sterlingclark <sterlingdclark13@gmail.com>
2020-05-05 12:01:11 -0400

    changed tab size and reformated

--- listing1-4.py
+++ listing1-4.py
@@ -7,6 +7,7 @@ HEIGHT = 600
 player_x = 550
 player_y = 350
 
+
 def draw():
     screen.blit(images.backdrop, (0, 0))
     screen.blit(images.mars, (50, 50))
@@ -16,12 +17,12 @@ def draw():
 def game_loop():
     global player_x, player_y
     if keyboard.right:
-            player_x += 5
+        player_x += 5
     elif keyboard.left:
-            player_x -= 5
+        player_x -= 5
     elif keyboard.up:
-            player_y -= 10
+        player_y -= 5
     elif keyboard.down:
-            player_y += 10
+        player_y += 5
 
 clock.schedule_interval(game_loop, 0.03)
