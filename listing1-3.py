6e77de6ddc258188655d702f4ed7c951bd1867b3
Sterlingclark <sterlingdclark13@gmail.com>
2020-05-04 23:12:14 -0400

    position changes of mars and ship

---
+++ listing1-3.py
@@ -0,0 +1,13 @@
+# Spacewalk 
+# Sterling Clark 
+# https://github.com/Sterlingclark
+
+WIDTH = 800
+HEIGHT = 600
+player_x = 500
+player_y = 550
+
+def draw():
+    screen.blit(images.backdrop, (0, 0))
+    screen.blit(images.ship, (130, 150))
+    screen.blit(images.mars, (50, 50))
